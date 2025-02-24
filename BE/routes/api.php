a67ce0d2a46651e786bf45f22cc8a8773a2c52d0
BreicheSinal <sinalbreiche@gmail.com>
2025-02-24 20:46:13 +0200

    Added profile endpoiint && business middleware

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckBusinessAuthorization;
 
 // AuthController routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,6 +11,14 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
+
+    Route::put('/profile/edit', [ProfileController::class, 'updateProfile']);
+
+    // Routes that require business authorization
+    Route::middleware(CheckBusinessAuthorization::class)->group(function () {
+        Route::put('/business/profile/edit', [ProfileController::class, 'updateProfile']);
+    });
 });
