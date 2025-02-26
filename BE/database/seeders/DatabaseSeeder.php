db0d866897db8d3eaf82996c9cb71e5ef7747aef
BreicheSinal <sinalbreiche@gmail.com>
2025-02-26 17:11:26 +0200

    Added activity type seeder

--- BE/database/seeders/DatabaseSeeder.php
+++ BE/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             RolesTableSeeder::class,
             CategoriesTableSeeder::class,
             AdminUserSeeder::class,
+            ActivityTypesTableSeeder::class,
         ]);
     }
 }
