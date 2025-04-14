e3bfc32f7466280d6c833cc3f68ed67192bcc01c
BreicheSinal <sinalbreiche@gmail.com>
2025-04-15 01:28:30 +0300

    Registered aws service

--- BE/app/Providers/AppServiceProvider.php
+++ BE/app/Providers/AppServiceProvider.php
@@ -11,7 +11,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('aws', function () {
+            return new \Aws\Sdk([
+                'credentials' => [
+                    'key'    => env('AWS_ACCESS_KEY_ID'),
+                    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                ],
+                'region' => env('AWS_DEFAULT_REGION'),
+                'version' => 'latest',
+            ]);
+        });
     }
 
     /**
