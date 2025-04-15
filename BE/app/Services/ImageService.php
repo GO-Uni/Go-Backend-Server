81eabb078f7a3ce26a3e918d4ca6c7c1556c426a
BreicheSinal <sinalbreiche@gmail.com>
2025-04-15 15:48:46 +0300

    Added uploading/deleting service for main and profile images

--- BE/app/Services/ImageService.php
+++ BE/app/Services/ImageService.php
@@ -44,6 +44,27 @@ class ImageService
         return $uploadedImages;
     }
 
+    public function uploadImage(UploadedFile $file, User $user): string
+    {
+        if (!$file->isValid()) {
+            throw new \Exception('Invalid file upload.');
+        }
+
+        $s3Path = $this->generateS3Path($file, $user->id);
+        $tempPath = $file->store('temp', 'public');
+
+        // Dispatch the job to upload the image to S3
+        UploadImageToS3::dispatch($tempPath, $s3Path);
+
+        return $s3Path;
+    }
+
+    public function deleteImage(array $imagePaths): void
+    {
+        // Dispatch the job to delete the images from S3
+        DeleteImageFromS3::dispatch($imagePaths);
+    }
+
     public function deleteImages(array $imageIds): void
     {
         $images = Image::whereIn('id', $imageIds)->get();
